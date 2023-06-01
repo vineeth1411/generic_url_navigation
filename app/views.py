0658a11a77455e92374a4c8ef1b8bf571e94a52f
=vineeth5774 <vineethpinnika@gmail.com>
2023-06-01 22:23:08 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def suneel(request):
+    return render(request,'suneel.html')
+
+def srujana(request):
+    return render(request, 'srujana.html')
